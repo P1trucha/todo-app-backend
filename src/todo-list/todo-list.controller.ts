63d573d5fecba2e384e059f29de4224a68f829d5
Piotr Mazepka <piotrmazepka@gmail.com>
2022-08-04 15:58:22 +0200

    feat: Create interface for todo-list

--- src/todo-list/todo-list.controller.ts
+++ src/todo-list/todo-list.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Inject, Param, Delete } from '@nestjs/common';
+import { TodoListInterface } from './Interfaces/TodoListInterface';
+import { TodoListService } from './todo-list.service';
+
+
 
 @Controller('todo-list')
-export class TodoListController {}
+export class TodoListController {
+
+
+    constructor(@Inject(TodoListService) private todoservice: TodoListService) { }
+
+    @Get('/all')
+    FindAll(): Promise<TodoListInterface[]> {
+        return this.todoservice.findAll();
+    }
+
+    @Get('/:id')
+    FindId(@Param('id') id: string): Promise<TodoListInterface> {
+        return this.todoservice.findOne(id);
+    }
+
+    @Delete('/:id')
+    DeleteId(@Param('id') id: string): void {
+        this.todoservice.deleteOne(id);
+    }
+
+
+
+}
